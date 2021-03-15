e693dbaf329ce06f625ef84a56fecfed51671d4f
Craig Curtis <ccurtis@unicon.net>
2021-03-15 16:15:28 -0700

    Add dotenv support

--- programs/dictionary-app/src/index.js
+++ programs/dictionary-app/src/index.js
@@ -1,6 +1,9 @@
 import app, { PORT } from "./app.js";
+import dotenv from "dotenv";
 import { log } from "clear-logs";
 
+dotenv.config();
+
 app.listen(PORT, () => {
-  log(`Ski Dictionary on port ${PORT}`);
+  log(`Ski Dictionary on port ${PORT}, ${process.env.TEST_VAR}`);
 });
